a4a79dfa8fe9589f3b54e90de786def3378ec433
Andrey Sumin <an.sumin@hh.ru>
2017-05-12 19:39:14 +0300

    make MicroHandler.make_url protected

--- frontik/micro_handler.py
+++ frontik/micro_handler.py
@@ -14,13 +14,13 @@ class MicroHandler(BaseHandler):
     _Request = namedtuple('_Request', ('method', 'host', 'uri', 'kwargs'))
 
     @staticmethod
-    def make_url(host, uri):
+    def _make_url(host, uri):
         return u'{}/{}'.format(host.rstrip(u'/'), uri.lstrip(u'/'))
 
     def GET(self, host, uri, data=None, headers=None, connect_timeout=None, request_timeout=None,
             follow_redirects=True, labels=None, fail_on_error=False):
         future = self._http_client.get_url(
-            self.make_url(host, uri),
+            self._make_url(host, uri),
             data=data, headers=headers,
             connect_timeout=connect_timeout, request_timeout=request_timeout,
             follow_redirects=follow_redirects, labels=labels,
@@ -33,7 +33,7 @@ class MicroHandler(BaseHandler):
              follow_redirects=True, content_type=None, labels=None, fail_on_error=False):
 
         future = self._http_client.post_url(
-            self.make_url(host, uri),
+            self._make_url(host, uri),
             data=data, headers=headers, files=files,
             connect_timeout=connect_timeout, request_timeout=request_timeout,
             follow_redirects=follow_redirects, content_type=content_type, labels=labels,
@@ -45,7 +45,7 @@ class MicroHandler(BaseHandler):
     def PUT(self, host, uri, data='', headers=None, connect_timeout=None, request_timeout=None,
             content_type=None, labels=None, fail_on_error=False):
         future = self._http_client.put_url(
-            self.make_url(host, uri),
+            self._make_url(host, uri),
             data=data, headers=headers,
             connect_timeout=connect_timeout, request_timeout=request_timeout,
             content_type=content_type, labels=labels,
@@ -57,7 +57,7 @@ class MicroHandler(BaseHandler):
     def DELETE(self, host, uri, data=None, headers=None, connect_timeout=None, request_timeout=None,
                content_type=None, labels=None, fail_on_error=False):
         future = self._http_client.delete_url(
-            self.make_url(host, uri),
+            self._make_url(host, uri),
             data=data, headers=headers,
             connect_timeout=connect_timeout, request_timeout=request_timeout,
             content_type=content_type, labels=labels,
